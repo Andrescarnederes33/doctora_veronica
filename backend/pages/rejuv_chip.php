6a7de1ac34c4885bab161199611cb1071b3608f5
frontend <frontend@cloudming.co>
2020-01-23 16:36:02 -0500

    ultimo commit 6.0

--- backend/pages/rejuv_chip.php
+++ backend/pages/rejuv_chip.php
@@ -22,12 +22,22 @@
         text-align: center
     }
 
-    #bg_aplication_rejuv_chip{
-        background-image: url(/public/src/img/);
+    #bg_aplication_rejuv_chip {
+        width: 100%;
+        height: 500px;
+        background-image: url(/public/src/img/section_rejuv_chip.png);
         background-size: cover;
         background-repeat: no-repeat
     }
 
+    .grid_doctora_presentation {
+        height: inherit;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+        grid-gap: 0;
+        color: #fff
+    }
 </style>
 <section id="fold_rejuv_chip">
     <div class="image_fold central_text_rejuv_chip">
@@ -48,7 +58,7 @@
     </div>
 </section>
 <div class="container">
-
+    <br>
     <section id="what_is_rejuv_chip">
         <h3>¿Que es Rejuv Chip?</h3>
         <p>
@@ -61,10 +71,24 @@
             modo podemos estar de vuelta a ese periodo de nuestras vidas donde nos encontramos con mayor vitalidad y
             energía, donde teníamos la pasión, la actitud y las ganas para lograr cualquier meta propuesta.
         </p>
+        <br>
+        <!-- <div> -->
+                <h3>Aplicación</h3>
+                <p>
+                    Poner el Rejuvchip es muy sencillo, para hacerlo primero se inyecta en la zona de la aplicación un poco
+                    de
+                    anestesia y luego se procede a hacer una incisión para insertar el Rejuvchip con un aplicador diseñado
+                    para
+                    este fin, es un proceso muy rápido entre dos y cinco minutos, tambien es muy sencillo, si se hace por un
+                    experto en la materia.
+                </p>
+    
+            <!-- </div> -->
+            <br>
     </section>
-
+    <br><br>
     <section id="solution_menopausia">
-        <h4>Rejuv Chip - Menopausia</h4>
+        <h3>Rejuv Chip - Menopausia</h3>
         <h5>¿Buscas tratar los síntomas de la menopausia?</h5>
         <p>
             Descubre una alternativa natural que trata eficientemente los síntomas:
@@ -72,6 +96,7 @@
         <a class="waves-effect waves-light btn-large"><i class="material-icons right"></i>Habla con la Doctora</a>
     </section>
     <br><br>
+    <br>
     <section id="benefits_rejuv_chip">
         <div class="row">
             <!-- <div class="col s12">This div is 12-columns wide on all screen sizes</div> -->
@@ -79,7 +104,7 @@
                 <img src="/public/src/img/servicios/beneficios_rejuv_chip.png" alt="" width="100%">
             </div>
             <div class="col m6 s12">
-                <h3>Beneficios del Rejuv Chip</h3>
+                <h4>Beneficios del Rejuv Chip</h4>
                 <ul>
                     <li class="list">Elimina el sofoco o bochorno ya que regula las sensaciones de temperatura.</li>
                     <br>
@@ -99,20 +124,37 @@
             </div>
         </div>
     </section>
-    <br><br>
-    <section id="aplication">
-        <div>
-            <h3>Aplicación</h3>
-                <p>
-                        Poner el Rejuvchip es muy sencillo, para hacerlo primero se inyecta en la zona de la aplicación un poco de
-                        anestesia y luego se procede a hacer una incisión para insertar el Rejuvchip con un aplicador diseñado para
-                        este fin, es un proceso muy rápido entre dos y cinco minutos, tambien es muy sencillo, si se hace por un
-                        experto en la materia.
-                    </p>
-            
-        </div>
-
-    </section>
+    
 </div>
 
-<div id="bg_aplication_rejuv_chip"></div>
+
+<section>
+    <div id="bg_aplication_rejuv_chip">
+
+        <div class="grid_doctora_presentation">
+
+            <div></div>
+            <div style="display: flex;flex-direction: column;align-items: flex-start;justify-content: center;">
+                <h3>DRA. Verónica Garcia Bedoya</h3>
+                <h4>Médica ginecóloga y obstetra</h4>
+                <p>
+                    La Doctora Verónica García Bedoya es una mujer apasionada por la asesoría y acompañamiento de la
+                    mujer
+                    en cada una de sus etapas de desarrollo involucrando temas como salud y vida sexual, maternidad,
+                    menopausia y adolescencia.
+                </p>
+                <a class="waves-effect waves-light btn-large"><i class="material-icons right">info</i>Más
+                    información</a>
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <div class="motivation_shop" style="padding: 70px 50px;">
+        <h3>Lista para tratar la menopausia y aumentar tu vitalidad de manera natural</h3>
+        <p>Con un clic recibe mas información de manera gratuita</p>
+        <a class="waves-effect waves-light btn-large"><i class="material-icons right">info</i>Agendar una cita</a>
+    </div>
+</section>
