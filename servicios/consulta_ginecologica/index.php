6fed91dbda8bcac9f55fa0872ac62f03b826bb6f
frontend <frontend@cloudming.co>
2020-01-24 00:54:11 -0500

    ultimo commit 16.0

--- servicios/consulta_ginecologica/index.php
+++ servicios/consulta_ginecologica/index.php
@@ -51,40 +51,6 @@
     <br>
     <br>
 
-    <section>
-        <div id="bg_aplication_rejuv_chip">
-
-            <div class="grid_doctora_presentation">
-
-                <div></div>
-                <div class="presentation_doctora">
-                    <h3>DRA. Verónica Garcia Bedoya</h3>
-                    <h4>Médica ginecóloga y obstetra</h4>
-                    <p>
-                        La Doctora Verónica García Bedoya es una mujer apasionada por la asesoría y acompañamiento
-                        de la
-                        mujer
-                        en cada una de sus etapas de desarrollo involucrando temas como salud y vida sexual,
-                        maternidad,
-                        menopausia y adolescencia.
-                    </p>
-                    <a class="waves-effect waves-light btn-large"><i class="material-icons right">info</i>Más
-                        información</a>
-                </div>
-
-            </div>
-
-        </div>
-
-
-        <!-- <div class="motivation_shop" style="padding: 70px 50px;">
-            <h3>Lista para tratar la menopausia y aumentar tu vitalidad de manera natural</h3>
-            <p>Con un clic recibe mas información de manera gratuita</p>
-            <a class="waves-effect waves-light btn-large"><i class="material-icons right">insert_invitation</i>Agendar una
-                cita</a>
-        </div> -->
-    </section>
-
     <?php include '../../backend/template_parts/footer.php'; ?>
 
     <!-- Compiled and minified JavaScript -->
